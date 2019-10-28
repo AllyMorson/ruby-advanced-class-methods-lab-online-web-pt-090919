b591c310da89a53cf4d020ce73dc8d6cacb97226
Ally Morson <ally.morson@gmail.com>
2019-10-28 02:19:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,68 @@
 class Song
+  attr_accessor :name , :artist_name 
+  @all = []
+  
+  def self.all
+    @all 
+  end 
+  
+  def save 
+    self.all.song << self 
+  end 
+  
+  def self.create 
+    song = Song.new 
+    song.save 
+    song 
+  end 
+  
+  def self.new_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song 
+  end 
+  
+  def self.create_by_name(song_name)
+    song = self.create 
+    song.name = song_name
+    song 
+  end 
+  
+  def self.find_by_name(song_name)
+    self.all.find {|s| s.name == song_name}
+  end 
+  
+  def find_or_create_by_name(song_name)
+    self.create_by_name(song_name) || self.find_by_name(song_name)
+  end 
+  
+  def self.alphabetical 
+    self.sort_by {|s| s.sort} 
+  end 
+  
+  def self.new_from_filename(filename)
+    parts = filename.split ("-")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+    
+    song = Song.new 
+    song = song_name
+    song.artist_name = artist_name
+    song 
+  end 
+  
+  def self.create_from_filename(filename)
+    parts = filename.split ("-")
+    artist_name = parts[0]
+    song_name = parts[1]
+    
+    song = self.create 
+    song.name = song_name
+    song.artist_name = artist_name
+    song 
+  end 
+  
+  def self.destoy_all
+    self.all.clear 
+  end 
   
